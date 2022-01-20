9ac696f6772e0ad470e77491a3e8da0aee0dfb98
eliasku <deadbabe@gmail.com>
2022-01-20 20:48:46 +0300

    up version

--- src/config/build_info.h
+++ src/config/build_info.h
@@ -2,7 +2,7 @@
 #ifndef APP_BUILD_INFO_H
 #define APP_BUILD_INFO_H
 
-#define APP_VERSION_NAME "1.5.12" 
-#define APP_VERSION_CODE "87" 
+#define APP_VERSION_NAME "1.5.13" 
+#define APP_VERSION_CODE "88" 
 
 #endif // APP_BUILD_INFO_H
