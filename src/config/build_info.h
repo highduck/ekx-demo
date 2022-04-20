870198027fec160eaa03090dd340d99213ce0875
eliasku <deadbabe@gmail.com>
2022-04-20 12:31:58 +0300

    upgrade 4

--- src/config/build_info.h
+++ src/config/build_info.h
@@ -2,7 +2,7 @@
 #ifndef APP_BUILD_INFO_H
 #define APP_BUILD_INFO_H
 
-#define APP_VERSION_NAME "1.5.16" 
-#define APP_VERSION_CODE "91" 
+#define APP_VERSION_NAME "1.5.17" 
+#define APP_VERSION_CODE "92" 
 
 #endif // APP_BUILD_INFO_H
