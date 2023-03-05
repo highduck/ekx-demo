d7bf2c1d37c3b0b87b072c985c9936b4f186808a
eliasku <deadbabe@gmail.com>
2023-03-05 21:50:47 +0300

    up

--- src/config/build_info.h
+++ src/config/build_info.h
@@ -2,7 +2,7 @@
 #ifndef APP_BUILD_INFO_H
 #define APP_BUILD_INFO_H
 
-#define APP_VERSION_NAME "1.5.19" 
-#define APP_VERSION_CODE "94" 
+#define APP_VERSION_NAME "1.5.20" 
+#define APP_VERSION_CODE "95" 
 
 #endif // APP_BUILD_INFO_H
